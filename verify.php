c39c3f60d565101e03b6869b0258040957e56639
ahmed yasser <106495602+ahmedyasse213@users.noreply.github.com>
2023-07-07 03:53:39 +0300

    verify.php

---
+++ verify.php
@@ -0,0 +1,8 @@
+<?php
+// Get the verification token from the URL
+$token = $_GET["token"];
+
+// Verify the token and update the user's account status
+
+// Redirect the user to the appropriate page
+?>
